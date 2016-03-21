2ad910a2e069a9c7ba11f7d850472cb082ad08ea
Shams Azeem <Shams Azeem>
2016-03-22 04:44:01 +0500

    Added loader for loading training classes file names

---
+++ smart_loader.py
@@ -0,0 +1,28 @@
+from __future__ import print_function
+import matplotlib.pyplot as plt
+import os
+import sys
+import tarfile
+import numpy as np
+import tensorflow as tf
+from IPython.display import display, Image
+from scipy import ndimage
+from sklearn.linear_model import LogisticRegression
+from six.moves.urllib.request import urlretrieve
+import logging
+
+def load_classes(root):
+    classes = np.array(sorted([os.path.join(root, directory) 
+                               for directory in os.listdir(root) 
+                               if os.path.isdir(os.path.join(root, directory))]))
+    
+    training_files = np.ndarray(shape=(len(classes),), dtype=object)
+
+    for index, path_prefix in enumerate(train_dirs):
+        temp_arr = np.array([os.path.join(path_prefix, filename)
+                             for filename in os.listdir(path_prefix) 
+                             if os.path.isfile(os.path.join(path_prefix, filename))])
+
+        training_files[index] = temp_arr    
+    
+    return training_files
