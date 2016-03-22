1fa82a4b3a64b861f950702369dbbfe2f0e196f3
Shams Azeem <Shams Azeem>
2016-03-22 05:54:07 +0500

    Shuffling data

--- smart_loader.py
+++ smart_loader.py
@@ -62,3 +62,10 @@ def merge_datasets(classes, train_size, valid_size=0):
         end_t += tsize_per_class
     
   return valid_dataset, valid_labels, train_dataset, train_labels
+
+np.random.seed(133)
+def randomize(dataset, labels):
+  permutation = np.random.permutation(labels.shape[0])
+  shuffled_dataset = dataset[permutation]
+  shuffled_labels = labels[permutation]
+  return shuffled_dataset, shuffled_labels
