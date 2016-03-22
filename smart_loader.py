72031725c1076dfa4ec69ce24c291a2998872088
Shams Azeem <Shams Azeem>
2016-03-22 05:49:55 +0500

    Merging required dataset

--- smart_loader.py
+++ smart_loader.py
@@ -28,3 +28,37 @@ def load_classes(root):
         labels[index] = np.array([index]*temp_arr.size)
     
     return classes, labels
+
+def make_arrays(nb_rows):
+  if nb_rows:
+    dataset = np.ndarray(nb_rows, dtype=object)
+    labels = np.ndarray(nb_rows, dtype=np.int32)
+  else:
+    dataset, labels = None, None
+  return dataset, labels
+
+def merge_datasets(classes, train_size, valid_size=0):
+  num_classes = len(classes)
+  valid_dataset, valid_labels = make_arrays(valid_size)
+  train_dataset, train_labels = make_arrays(train_size)
+  vsize_per_class = valid_size // num_classes
+  tsize_per_class = train_size // num_classes
+    
+  start_v, start_t = 0, 0
+  end_v, end_t = vsize_per_class, tsize_per_class
+  end_l = vsize_per_class+tsize_per_class
+  for label, class_ in enumerate(classes):
+    if valid_dataset is not None:
+        valid_letter = class_[:vsize_per_class]
+        valid_dataset[start_v:end_v] = valid_letter
+        valid_labels[start_v:end_v] = label
+        start_v += vsize_per_class
+        end_v += vsize_per_class
+                    
+        train_letter = class_[vsize_per_class:end_l]
+        train_dataset[start_t:end_t] = train_letter
+        train_labels[start_t:end_t] = label
+        start_t += tsize_per_class
+        end_t += tsize_per_class
+    
+  return valid_dataset, valid_labels, train_dataset, train_labels
