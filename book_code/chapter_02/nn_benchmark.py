62523fa2b1c3239afae501a0c0f748982bd883ad
Shams Azeem <Shams Azeem>
2016-04-15 20:25:40 +0500

    Attempting to fix import error # 2

--- book_code/chapter_02/nn_benchmark.py
+++ book_code/chapter_02/nn_benchmark.py
@@ -1,7 +1,9 @@
 import sys, os
 
+print(os.path.realpath('..'))
+print(__file__)
 sys.path.append(os.path.realpath('..'))
-                
+
 from book_code.data_utils import *
 
 not_mnist, image_size, num_of_classes = prepare_not_mnist_dataset()
