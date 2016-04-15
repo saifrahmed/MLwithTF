a288d72940b609d783e476bf8071b4c63816be5e
Shams Azeem <Shams Azeem>
2016-04-15 20:28:43 +0500

    Attempting to fix import error # 3

--- book_code/chapter_02/nn_benchmark.py
+++ book_code/chapter_02/nn_benchmark.py
@@ -1,8 +1,8 @@
 import sys, os
 
-print(os.path.realpath('..'))
+print(os.path.realpath('../..'))
 print(__file__)
-sys.path.append(os.path.realpath('..'))
+sys.path.append(os.path.realpath('../..'))
 
 from book_code.data_utils import *
 
