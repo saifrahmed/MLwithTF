0446b5e24a7671127639621c2246417efa812cbf
Shams Azeem <Shams Azeem>
2016-04-19 04:32:40 +0500

    Removed unnecessary files and prepared chapter 2 and chapter 3 code for taking in multiple datasets

--- book_code/chapter_02/nn_benchmark.py
+++ book_code/chapter_02/nn_benchmark.py
@@ -11,10 +11,13 @@ num_steps = 6001
 learning_rate = 0.3
 relu_layers = 64
 
-def reformat(data, image_size, num_of_classes):
-    data.train_dataset = data.train_dataset.reshape((-1, image_size * image_size)).astype(np.float32)
-    data.valid_dataset = data.valid_dataset.reshape((-1, image_size * image_size)).astype(np.float32)
-    data.test_dataset = data.test_dataset.reshape((-1, image_size * image_size)).astype(np.float32)
+data_showing_step = 500
+
+
+def reformat(data, image_size, num_of_channels, num_of_classes):
+    data.train_dataset = data.train_dataset.reshape((-1, image_size * image_size * num_of_channels)).astype(np.float32)
+    data.valid_dataset = data.valid_dataset.reshape((-1, image_size * image_size * num_of_channels)).astype(np.float32)
+    data.test_dataset = data.test_dataset.reshape((-1, image_size * image_size * num_of_channels)).astype(np.float32)
 
     # Map 0 to [1.0, 0.0, 0.0 ...], 1 to [0.0, 1.0, 0.0 ...]
     data.train_labels = (np.arange(num_of_classes) == data.train_labels[:, None]).astype(np.float32)
@@ -36,26 +39,26 @@ def nn_model(data, weights, biases):
     return tf.matmul(relu_layer, weights['fc2']) + biases['fc2']
 
 
-not_mnist, image_size, num_of_classes = prepare_not_mnist_dataset()
-not_mnist = reformat(not_mnist, image_size, num_of_classes)
+dataset, image_size, num_of_classes, num_of_channels = prepare_not_mnist_dataset()
+dataset = reformat(dataset, image_size, num_of_channels, num_of_classes)
 
-print('Training set', not_mnist.train_dataset.shape, not_mnist.train_labels.shape)
-print('Validation set', not_mnist.valid_dataset.shape, not_mnist.valid_labels.shape)
-print('Test set', not_mnist.test_dataset.shape, not_mnist.test_labels.shape)
+print('Training set', dataset.train_dataset.shape, dataset.train_labels.shape)
+print('Validation set', dataset.valid_dataset.shape, dataset.valid_labels.shape)
+print('Test set', dataset.test_dataset.shape, dataset.test_labels.shape)
 
 graph = tf.Graph()
 with graph.as_default():
     # Input data. For the training data, we use a placeholder that will be fed
     # at run time with a training minibatch.
     tf_train_dataset = tf.placeholder(tf.float32,
-                                      shape=(batch_size, image_size * image_size))
+                                      shape=(batch_size, image_size * image_size * num_of_channels))
     tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size, num_of_classes))
-    tf_valid_dataset = tf.constant(not_mnist.valid_dataset)
-    tf_test_dataset = tf.constant(not_mnist.test_dataset)
+    tf_valid_dataset = tf.constant(dataset.valid_dataset)
+    tf_test_dataset = tf.constant(dataset.test_dataset)
 
     # Variables.
     weights = {
-        'fc1': tf.Variable(tf.truncated_normal([image_size * image_size, num_of_classes])),
+        'fc1': tf.Variable(tf.truncated_normal([image_size * image_size * num_of_channels, num_of_classes])),
         'fc2': tf.Variable(tf.truncated_normal([num_of_classes, num_of_classes]))
     }
     biases = {
@@ -84,18 +87,18 @@ with tf.Session(graph=graph) as session:
         sys.stdout.flush()
         # Pick an offset within the training data, which has been randomized.
         # Note: we could use better randomization across epochs.
-        offset = (step * batch_size) % (not_mnist.train_labels.shape[0] - batch_size)
+        offset = (step * batch_size) % (dataset.train_labels.shape[0] - batch_size)
         # Generate a minibatch.
-        batch_data = not_mnist.train_dataset[offset:(offset + batch_size), :]
-        batch_labels = not_mnist.train_labels[offset:(offset + batch_size), :]
+        batch_data = dataset.train_dataset[offset:(offset + batch_size), :]
+        batch_labels = dataset.train_labels[offset:(offset + batch_size), :]
         # Prepare a dictionary telling the session where to feed the minibatch.
         # The key of the dictionary is the placeholder node of the graph to be fed,
         # and the value is the numpy array to feed to it.
         feed_dict = {tf_train_dataset: batch_data, tf_train_labels: batch_labels}
         _, l, predictions = session.run(
             [optimizer, loss, train_prediction], feed_dict=feed_dict)
-        if (step % 500 == 0):
+        if (step % data_showing_step == 0):
             logger.info('Step %03d  Acc Minibatch: %03.2f%%  Acc Val: %03.2f%%  Minibatch loss %f' % (
                 step, accuracy(predictions, batch_labels), accuracy(
-                valid_prediction.eval(), not_mnist.valid_labels), l))
-    print("Test accuracy: %.1f%%" % accuracy(test_prediction.eval(), not_mnist.test_labels))
+                valid_prediction.eval(), dataset.valid_labels), l))
+    print("Test accuracy: %.1f%%" % accuracy(test_prediction.eval(), dataset.test_labels))
--- book_code/chapter_03/nn_benchmark_with_tensorboard.py
+++ book_code/chapter_03/nn_benchmark_with_tensorboard.py
@@ -11,12 +11,15 @@ num_steps = 6001
 learning_rate = 0.3
 relu_layers = 1
 
+data_showing_step = 500
+
 log_location = '/tmp/simple_nn_log'
 
-def reformat(data, image_size, num_of_classes):
-    data.train_dataset = data.train_dataset.reshape((-1, image_size * image_size)).astype(np.float32)
-    data.valid_dataset = data.valid_dataset.reshape((-1, image_size * image_size)).astype(np.float32)
-    data.test_dataset = data.test_dataset.reshape((-1, image_size * image_size)).astype(np.float32)
+
+def reformat(data, image_size, num_of_channels, num_of_classes):
+    data.train_dataset = data.train_dataset.reshape((-1, image_size * image_size * num_of_channels)).astype(np.float32)
+    data.valid_dataset = data.valid_dataset.reshape((-1, image_size * image_size * num_of_channels)).astype(np.float32)
+    data.test_dataset = data.test_dataset.reshape((-1, image_size * image_size * num_of_channels)).astype(np.float32)
 
     # Map 0 to [1.0, 0.0, 0.0 ...], 1 to [0.0, 1.0, 0.0 ...]
     data.train_labels = (np.arange(num_of_classes) == data.train_labels[:, None]).astype(np.float32)
@@ -30,8 +33,8 @@ def accuracy(predictions, labels):
     return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1))
             / predictions.shape[0])
 
-def nn_model(data, weights, biases):
 
+def nn_model(data, weights, biases):
     with tf.name_scope('FC_layer_1') as scope:
         layer_fc1 = tf.nn.bias_add(tf.matmul(data, weights['fc1']), biases['fc1'], name=scope)
     relu_layer = tf.nn.relu(layer_fc1)
@@ -42,26 +45,28 @@ def nn_model(data, weights, biases):
     return layer_fc2
 
 
-not_mnist, image_size, num_of_classes = prepare_not_mnist_dataset()
-not_mnist = reformat(not_mnist, image_size, num_of_classes)
+dataset, image_size, num_of_classes, num_of_channels = prepare_not_mnist_dataset()
+dataset = reformat(dataset, image_size, num_of_channels, num_of_classes)
 
-print('Training set', not_mnist.train_dataset.shape, not_mnist.train_labels.shape)
-print('Validation set', not_mnist.valid_dataset.shape, not_mnist.valid_labels.shape)
-print('Test set', not_mnist.test_dataset.shape, not_mnist.test_labels.shape)
+print('Training set', dataset.train_dataset.shape, dataset.train_labels.shape)
+print('Validation set', dataset.valid_dataset.shape, dataset.valid_labels.shape)
+print('Test set', dataset.test_dataset.shape, dataset.test_labels.shape)
 
 graph = tf.Graph()
 with graph.as_default():
     # Input data. For the training data, we use a placeholder that will be fed
     # at run time with a training minibatch.
     tf_train_dataset = tf.placeholder(tf.float32,
-                                      shape=(batch_size, image_size * image_size), name='TRAIN_DATASET')
+                                      shape=(batch_size, image_size * image_size * num_of_channels),
+                                      name='TRAIN_DATASET')
     tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size, num_of_classes), name='TRAIN_LABEL')
-    tf_valid_dataset = tf.constant(not_mnist.valid_dataset, name='VALID_DATASET')
-    tf_test_dataset = tf.constant(not_mnist.test_dataset, name='TEST_DATASET')
+    tf_valid_dataset = tf.constant(dataset.valid_dataset, name='VALID_DATASET')
+    tf_test_dataset = tf.constant(dataset.test_dataset, name='TEST_DATASET')
 
     # Variables.
     weights = {
-        'fc1': tf.Variable(tf.truncated_normal([image_size * image_size, num_of_classes]), name='weights'),
+        'fc1': tf.Variable(tf.truncated_normal([image_size * image_size * num_of_channels, num_of_classes]),
+                           name='weights'),
         'fc2': tf.Variable(tf.truncated_normal([num_of_classes, num_of_classes]), name='weights')
     }
     biases = {
@@ -102,10 +107,10 @@ with tf.Session(graph=graph) as session:
         sys.stdout.flush()
         # Pick an offset within the training data, which has been randomized.
         # Note: we could use better randomization across epochs.
-        offset = (step * batch_size) % (not_mnist.train_labels.shape[0] - batch_size)
+        offset = (step * batch_size) % (dataset.train_labels.shape[0] - batch_size)
         # Generate a minibatch.
-        batch_data = not_mnist.train_dataset[offset:(offset + batch_size), :]
-        batch_labels = not_mnist.train_labels[offset:(offset + batch_size), :]
+        batch_data = dataset.train_dataset[offset:(offset + batch_size), :]
+        batch_labels = dataset.train_labels[offset:(offset + batch_size), :]
         # Prepare a dictionary telling the session where to feed the minibatch.
         # The key of the dictionary is the placeholder node of the graph to be fed,
         # and the value is the numpy array to feed to it.
@@ -115,8 +120,8 @@ with tf.Session(graph=graph) as session:
 
         writer.add_summary(summary_result, step)
 
-        if (step % 500 == 0):
+        if (step % data_showing_step == 0):
             logger.info('Step %03d  Acc Minibatch: %03.2f%%  Acc Val: %03.2f%%  Minibatch loss %f' % (
                 step, accuracy(predictions, batch_labels), accuracy(
-                valid_prediction.eval(), not_mnist.valid_labels), l))
-    print("Test accuracy: %.1f%%" % accuracy(test_prediction.eval(), not_mnist.test_labels))
+                valid_prediction.eval(), dataset.valid_labels), l))
+    print("Test accuracy: %.1f%%" % accuracy(test_prediction.eval(), dataset.test_labels))
