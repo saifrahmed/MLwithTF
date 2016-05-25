acd442000838387134b1ad678d5be5b633226a7f
ShamsUlAzeem <shamsazeem20@gmail.com>
2016-05-25 23:29:57 +0500

    Reformated cifar10 shape and now the features are training better than before

--- book_code/chapter_04/test2.py
+++ book_code/chapter_04/test2.py
@@ -14,13 +14,14 @@ current_pickle_file = cifar_data_folder + '/' + train_pickle_files[0]
 
 log_location = '/home/shams/Desktop/pycharm_projects/cifar-10_improv/alex_nn_log'
 batch_size = 32
-learning_rate = 1.0
+learning_rate = 0.02
 num_of_classes = 10
 SEED = 11215
-stddev = 0.01
-stddev_fc = 0.005
+stddev = 0.1
+stddev_fc = 0.05
 data_showing_step = 50
 num_steps = 100001
+regularization_factor = 0   #5e-4
 
 
 def load_cifar_10_pickle(pickle_file):
@@ -60,6 +61,36 @@ def load_cifar_10_from_pickles(train_pickle_files, test_pickle_files, pickle_bat
     return all_train_data, all_train_labels, all_test_data, all_test_labels
 
 
+def reshape_linear(data, shape):
+    output = np.ndarray(shape=(data.shape[0], shape, shape, 3), dtype=np.float32)
+    for index, image in enumerate(data):
+        sys.stdout.write('Reshaping image %d of %d                    \r' % (index + 1, data.shape[0]))
+        sys.stdout.flush()
+        for row in range(shape):
+            for col in range(shape):
+                image_index = row * shape + col
+                output[index, row, col, :] = [image[image_index],
+                                              image[shape**2 + image_index],
+                                              image[(shape**2)*2 + image_index]]
+    return output
+
+def reshape_linear_fast(data, shape, channels):
+    linear_pixels = shape**2
+    sequence = np.ndarray(shape=(linear_pixels*channels), dtype=np.int32)
+    for i in range(linear_pixels):
+        start = i * (channels)
+        end = (i+1) * (channels)
+        sequence[start:end] = range(i, linear_pixels*channels, linear_pixels)
+    return data[:, sequence].reshape(-1, shape, shape, channels)
+
+
+def reshape_labels(data, classes):
+    return (np.arange(classes) == data[:, None]).astype(np.float32)
+
+def normalize(data):
+    return (data - 255/2) / 255
+
+
 def accuracy(predictions, labels):
     return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1))
             / predictions.shape[0])
@@ -67,7 +98,7 @@ def accuracy(predictions, labels):
 
 def nn_model(data, weights, biases, TRAIN=False):
     with tf.name_scope('Layer_1') as scope:
-        conv = tf.nn.conv2d(data, weights['conv1'], strides=[1, 4, 4, 1], padding='SAME', name='conv1')
+        conv = tf.nn.conv2d(data, weights['conv1'], strides=[1, 2, 2, 1], padding='SAME', name='conv1')
         bias_add = tf.nn.bias_add(conv, biases['conv1'], name='bias_add_1')
         relu = tf.nn.relu(bias_add, name='relu_1')
         lrn = tf.nn.lrn(relu, 5, bias=1.0, alpha=0.0001, beta=0.75, name='lrn1')
@@ -83,17 +114,7 @@ def nn_model(data, weights, biases, TRAIN=False):
     with tf.name_scope('Layer_3') as scope:
         conv = tf.nn.conv2d(max_pool, weights['conv3'], strides=[1, 1, 1, 1], padding='SAME', name='conv3')
         bias_add = tf.nn.bias_add(conv, biases['conv3'], name='bias_add_3')
-        relu = tf.nn.relu(bias_add, name=scope)
-
-    with tf.name_scope('Layer_4') as scope:
-        conv = tf.nn.conv2d(relu, weights['conv4'], strides=[1, 1, 1, 1], padding='SAME', name='conv4')
-        bias_add = tf.nn.bias_add(conv, biases['conv4'], name='bias_add_4')
-        relu = tf.nn.relu(bias_add, name=scope)
-
-    with tf.name_scope('Layer_5') as scope:
-        conv = tf.nn.conv2d(relu, weights['conv5'], strides=[1, 1, 1, 1], padding='SAME', name='conv5')
-        bias_add = tf.nn.bias_add(conv, biases['conv5'], name='bias_add_5')
-        relu = tf.nn.relu(bias_add, name='relu_5')
+        relu = tf.nn.relu(bias_add, name='relu_3')
         max_pool = tf.nn.max_pool(relu, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='VALID', name=scope)
 
     shape = max_pool.get_shape().as_list()
@@ -110,105 +131,65 @@ def nn_model(data, weights, biases, TRAIN=False):
         matmul = tf.matmul(relu, weights['fc7'], name='fc7_matmul')
         bias_add = tf.nn.bias_add(matmul, biases['fc7'], name='fc7_bias_add')
         relu = tf.nn.relu(bias_add, name=scope)
-        if (TRAIN):
-            relu = tf.nn.dropout(relu, 0.5, seed=SEED, name='dropout_fc7')
 
-    with tf.name_scope('FC_Layer_8') as scope:
-        matmul = tf.matmul(relu, weights['fc8'], name='fc8_matmul')
-        bias_add = tf.nn.bias_add(matmul, biases['fc8'], name='fc8_bias_add')
-        relu = tf.nn.relu(bias_add, name=scope)
-        if (TRAIN):
-            relu = tf.nn.dropout(relu, 0.5, seed=SEED, name='dropout_fc8')
-
-    with tf.name_scope('FC_Layer_9') as scope:
-        matmul = tf.matmul(relu, weights['fc9'], name='fc9_matmul')
-        layer_fc9 = tf.nn.bias_add(matmul, biases['fc9'], name=scope)
-
-    return layer_fc9
+    return relu
 
 data, labels, test_data, test_labels = load_cifar_10_from_pickles([cifar_data_folder + '/' + x for x in
                                                                    train_pickle_files],
                                                                   [cifar_data_folder + '/' + x for x in
                                                                    test_pickle_files], 10000, 32, 3)
 
-train_data = data[0:45000]
-train_labels = labels[0:45000]
-valid_data = data[45000:50000]
-valid_labels = labels[45000:50000]
+train_data = normalize(reshape_linear_fast(data[0:45000], 32, 3))
+train_labels = reshape_labels(labels[0:45000], 10)
+valid_data = normalize(reshape_linear_fast(data[45000:50000], 32, 3))
+valid_labels = reshape_labels(labels[45000:50000], 10)
+test_data = normalize(reshape_linear_fast(test_data, 32, 3))
+test_labels = reshape_labels(test_labels, 10)
+
+#for i in range(1000):
+#    imsave(test_images_folder + '/image_train' + str(i) + '.png', train_data[i])
+#    imsave(test_images_folder + '/image_valid' + str(i) + '.png', valid_data[i])
+#    imsave(test_images_folder + '/image_test' + str(i) + '.png', test_data[i])
+
+
+print('saved')
 
 print(train_data.shape, train_labels.shape, valid_data.shape, valid_labels.shape, test_data.shape, test_labels.shape)
-
-#for i in range(20):
-#    reshapedImage = data[i].reshape((3, 32, 32))
-#    types = ['nearest', 'bilinear', 'bicubic', 'cubic']
-#    for type in types:
-#        resizedImage = imresize(reshapedImage, (227, 227, 3), interp=type)
-#        imsave(test_images_folder + '/image' + str(i) + '_' + type + '.png', resizedImage)
-#        reformatedImage = resizedImage.astype(float)
-#    print(i)
-
-validReshapedImages = valid_data[0:100].reshape((-1, 3, 32, 32))
-validResizedImages = np.array([imresize(validReshapedImage, (227, 227, 3), interp='bicubic').astype(np.float32)
-                          for validReshapedImage in validReshapedImages])
-validDataToUse = (validResizedImages - 255 / 2) / 255
-validLabelsToUse = (np.arange(num_of_classes) == valid_labels[0:100][:, None]).astype(np.float32)
-
-testReshapedImages = test_data[0:200].reshape((-1, 3, 32, 32))
-testResizedImages = np.array([imresize(testReshapedImage, (227, 227, 3), interp='bicubic').astype(np.float32)
-                          for testReshapedImage in testReshapedImages])
-testDataToUse = (testResizedImages - 255 / 2) / 255
-testLabelsToUse = (np.arange(num_of_classes) == test_labels[0:200][:, None]).astype(np.float32)
 
 graph = tf.Graph()
 with graph.as_default():
     tf_train_dataset = tf.placeholder(tf.float32,
-                                      shape=(batch_size, 227, 227, 3), name='TRAIN_DATASET')
+                                      shape=(batch_size, 32, 32, 3), name='TRAIN_DATASET')
     tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size, num_of_classes), name='TRAIN_LABEL')
-    tf_valid_dataset = tf.constant(validDataToUse, tf.float32,
-                                      shape=(100, 227, 227, 3), name='VALID_DATASET')
-    tf_test_dataset = tf.constant(testDataToUse, tf.float32,
-                                      shape=(200, 227, 227, 3), name='TEST_DATASET')
+    tf_valid_dataset = tf.constant(valid_data, tf.float32,
+                                      shape=(valid_data.shape[0], 32, 32, 3), name='VALID_DATASET')
+    tf_test_dataset = tf.constant(test_data, tf.float32,
+                                      shape=(test_data.shape[0], 32, 32, 3), name='TEST_DATASET')
 
     # Variables.
     weights = {
-        'conv1': tf.Variable(tf.truncated_normal([11, 11, 3, 96], dtype=tf.float32,
+        'conv1': tf.Variable(tf.truncated_normal([3, 3, 3, 64], dtype=tf.float32,
                                                  stddev=stddev, seed=SEED), name='weights'),
-        'conv2': tf.Variable(tf.truncated_normal([5, 5, 96, 256], dtype=tf.float32,
+        'conv2': tf.Variable(tf.truncated_normal([3, 3, 64, 194], dtype=tf.float32,
                                                  stddev=stddev, seed=SEED), name='weights'),
-        'conv3': tf.Variable(tf.truncated_normal([3, 3, 256, 384], dtype=tf.float32,
+        'conv3': tf.Variable(tf.truncated_normal([1, 1, 194, 256], dtype=tf.float32,
                                                  stddev=stddev, seed=SEED), name='weights'),
-        'conv4': tf.Variable(tf.truncated_normal([3, 3, 384, 384], dtype=tf.float32,
-                                                 stddev=stddev, seed=SEED), name='weights'),
-        'conv5': tf.Variable(tf.truncated_normal([3, 3, 384, 256], dtype=tf.float32,
-                                                 stddev=stddev, seed=SEED), name='weights'),
-        'fc6': tf.Variable(tf.truncated_normal([9216, 4096], dtype=tf.float32,
+        'fc6': tf.Variable(tf.truncated_normal([256, 128], dtype=tf.float32,
                                                stddev=stddev_fc, seed=SEED), name='weights'),
-        'fc7': tf.Variable(tf.truncated_normal([4096, 4096], dtype=tf.float32,
-                                               stddev=stddev_fc, seed=SEED), name='weights'),
-        'fc8': tf.Variable(tf.truncated_normal([4096, 1000], dtype=tf.float32,
-                                               stddev=stddev, seed=SEED), name='weights'),
-        'fc9': tf.Variable(tf.truncated_normal([1000, num_of_classes], dtype=tf.float32,
-                                               stddev=stddev, seed=SEED), name='weights')
+        'fc7': tf.Variable(tf.truncated_normal([128, num_of_classes], dtype=tf.float32,
+                                               stddev=stddev_fc, seed=SEED), name='weights')
     }
     biases = {
-        'conv1': tf.Variable(tf.constant(0.1, shape=[96], dtype=tf.float32),
+        'conv1': tf.Variable(tf.constant(0.1, shape=[64], dtype=tf.float32),
                          trainable=True, name='biases'),
-        'conv2': tf.Variable(tf.constant(0.1, shape=[256], dtype=tf.float32),
+        'conv2': tf.Variable(tf.constant(0.1, shape=[194], dtype=tf.float32),
                          trainable=True, name='biases'),
-        'conv3': tf.Variable(tf.constant(0.1, shape=[384], dtype=tf.float32),
+        'conv3': tf.Variable(tf.constant(0.1, shape=[256], dtype=tf.float32),
                          trainable=True, name='biases'),
-        'conv4': tf.Variable(tf.constant(0.1, shape=[384], dtype=tf.float32),
+        'fc6': tf.Variable(tf.constant(0.1, shape=[128], dtype=tf.float32),
                          trainable=True, name='biases'),
-        'conv5': tf.Variable(tf.constant(0.1, shape=[256], dtype=tf.float32),
+        'fc7': tf.Variable(tf.constant(0.1, shape=[num_of_classes], dtype=tf.float32),
                          trainable=True, name='biases'),
-        'fc6': tf.Variable(tf.constant(0.1, shape=[4096], dtype=tf.float32),
-                         trainable=True, name='biases'),
-        'fc7': tf.Variable(tf.constant(0.1, shape=[4096], dtype=tf.float32),
-                         trainable=True, name='biases'),
-        'fc8': tf.Variable(tf.constant(0.1, shape=[1000], dtype=tf.float32),
-                         trainable=True, name='biases'),
-        'fc9': tf.Variable(tf.constant(0.1, shape=[num_of_classes], dtype=tf.float32),
-                         trainable=True, name='biases')
     }
 
     for weight_key in sorted(weights.keys()):
@@ -224,11 +205,9 @@ with graph.as_default():
 
     # L2 regularization for the fully connected parameters.
     regularizers = (tf.nn.l2_loss(weights['fc6']) + tf.nn.l2_loss(biases['fc6']) +
-                    tf.nn.l2_loss(weights['fc7']) + tf.nn.l2_loss(biases['fc7']) +
-                    tf.nn.l2_loss(weights['fc8']) + tf.nn.l2_loss(biases['fc8']) +
-                    tf.nn.l2_loss(weights['fc9']) + tf.nn.l2_loss(biases['fc9']))
+                    tf.nn.l2_loss(weights['fc7']) + tf.nn.l2_loss(biases['fc7']))
     # Add the regularization term to the loss.
-    loss += 5e-4 * regularizers
+    loss += regularization_factor * regularizers
 
     _ = tf.scalar_summary('nn_loss', loss)
 
@@ -254,13 +233,7 @@ with tf.Session(graph=graph) as session:
         offset = (step * batch_size) % (train_labels.shape[0] - batch_size)
         # Generate a minibatch.
         batch_data = train_data[offset:(offset + batch_size), :]
-        reshapedImages = batch_data.reshape((-1, 3, 32, 32))
-        resizedImages = np.array([imresize(reshapedImage, (227, 227, 3), interp='bicubic').astype(np.float32)
-                         for reshapedImage in reshapedImages])
-        #for index, resizedImage in enumerate(resizedImages):
-        #    imsave(test_images_folder + '/image' + str(index) + '_bicubic.png', resizedImage)
-        batch_data = (resizedImages - 255/2) / 255
-        batch_labels = (np.arange(num_of_classes) == train_labels[offset:(offset + batch_size)][:, None]).astype(np.float32)
+        batch_labels = train_labels[offset:(offset + batch_size), :]
         #print(np.argmax(batch_labels, 1))
         feed_dict = {tf_train_dataset: batch_data, tf_train_labels: batch_labels}
         # print feed_dict
@@ -271,6 +244,6 @@ with tf.Session(graph=graph) as session:
 
         if (step % data_showing_step == 0):
             print('Step %03d  Acc-Minibatch: %03.2f%% Acc-Valid: %03.2f%% Minibatch loss %f' %
-                  (step, accuracy(predictions, batch_labels), accuracy(valid_prediction.eval(), validLabelsToUse), l))
+                  (step, accuracy(predictions, batch_labels), accuracy(valid_prediction.eval(), valid_labels), l))
 
-    print('Acc-Test: %03.2f%%' % accuracy(test_prediction.eval(), testLabelsToUse))
+    print('Acc-Test: %03.2f%%' % accuracy(test_prediction.eval(), test_labels))
